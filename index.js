9d72c859fbd8c39514c0732751e6d249d8cb6cef
roger rodriguez <rogerdavid.rd@gmail.com>
2023-05-10 01:30:55 -0400

    primer commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import { argv } from "node:process";
+import { readCitas, register, deleteCitas, updatedRegister } from "./operations.js";
+
+// try {
+//     await writeFile("operations.js", "");
+//     console.log(' archivo JS creado ');
+
+// } catch (error) {
+//     console.log("el JS no se pudo crear");
+// }
+// try {
+//     await writeFile("citas.json", JSON.stringify([]));
+//     console.log("archivo JSON creado")
+// } catch (error) {
+//     console.log('el JSON no se pudo crear')
+// }
+
+const [acc, NombreDelAnimal, Edad, TipoDeAnimal, ColorDelAnimal, Enfermedad] =
+    argv.slice(2);
+
+if (acc === "register"){
+    register(NombreDelAnimal, Edad, TipoDeAnimal, ColorDelAnimal, Enfermedad);
+}
+
+if (acc === "read"){
+    readCitas();
+}
+
+if(acc !== "register" ||"read"){
+    console.log(`ERROR en comando:${acc},`,"debes ingresar un comando: read, register." )
+}
---
+++ operations.js
@@ -0,0 +1,37 @@
+import { nanoid } from "nanoid";
+import { readFile, writeFile } from "node:fs/promises";
+
+export const register = async (
+    NombreDelAnimal,
+    Edad,
+    TipoDeAnimal,
+    ColorDelAnimal,
+    Enfermedad
+) => {
+  try {
+    const records = JSON.parse(await readFile("citas.json"));
+    const record = {
+        id: nanoid(),
+        NombreDelAnimal,
+        Edad,
+        TipoDeAnimal,
+        ColorDelAnimal,
+        Enfermedad,
+    };
+    records.push(record);
+    await writeFile("citas.json", JSON.stringify(records));
+    console.log("se a creado el perfil del animal");
+    } catch (error) {
+    console.log(error);
+}
+};
+
+export const readCitas = async () => {
+  try {
+    const records = JSON.parse(await readFile("citas.json","utf-8"));
+    console.log(records);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
